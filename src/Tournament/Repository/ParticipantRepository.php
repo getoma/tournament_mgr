5f06be0e8c54dc121ad38a4139e61a715f26425f
getoma <getoma@email.de>
2026-02-05 09:59:39 +0100

    Fix participant update in repo code

--- src/Tournament/Repository/ParticipantRepository.php
+++ src/Tournament/Repository/ParticipantRepository.php
@@ -222,30 +222,40 @@ class ParticipantRepository
 
       if( $result ) // also update participant categories
       {
-         /* ..by first deleting all assignments, and adding them again afterwards, including the (possibly) updated pre_assignment
-          * we are talking about less than 3 entries here that get deleted and re-added, so doing a query and generating delta-updates
-          * is hardly worth the effort.
-          */
-         $sql = "DELETE FROM participants_categories WHERE participant_id = ?";
-         $params = [$p->id];
-         $stmt = $this->pdo->prepare($sql);
-         $result = $stmt->execute($params);
-
-         if( !$p->categories->empty() )
+         if ($p->categories->empty())
          {
-            // then add all linked category ids, respectively update the pre_assign values
-            $values = [];
-            $params = [];
-            foreach ($p->categories as $cat_assign)
-            {
-               $values[] = "(?,?,?)";
-               $params[] = $p->id;
-               $params[] = $cat_assign->category->id;
-               $params[] = $cat_assign->pre_assign;
-            }
-            $sql = "INSERT INTO participants_categories (participant_id, category_id, pre_assign) VALUES " . implode(',', $values);
+            // If no categories set (anymore), just delete all assigments
+            $stmt = $this->pdo->prepare("DELETE FROM participants_categories WHERE participant_id = ?");
+            $result = $stmt->execute([$p->id]);
+         }
+         else
+         {
+            // otherwise, delete any category assignments that are not set (anymore)
+            // DO NOT just delete everything and re-add, because that would also drop any
+            // related data (e.g. start slot assignments)
+            $placeholders = implode(',', array_fill(0, $p->categories->count(), '?'));
+            $sql = "DELETE FROM participants_categories WHERE participant_id = ? AND category_id NOT IN ($placeholders)";
+            $params = array_merge([$p->id], $p->categories->map(fn($ca) => $ca->category->id));
             $stmt = $this->pdo->prepare($sql);
-            $result = $stmt->execute($params) && $result;
+            $stmt->execute($params);
+
+            // then add all linked category ids, respectively update the pre_assign values
+            $stmt = $this->pdo->prepare(<<<'QUERY'
+            INSERT INTO participants_categories (participant_id, category_id, pre_assign) VALUES (:pid, :cid, :set_assign)
+            ON DUPLICATE KEY UPDATE pre_assign=:update_assign
+QUERY);
+            /** @var CategoryAssignment $ca */
+            foreach ($p->categories as $ca)
+            {
+               $result = $stmt->execute([
+                  ':pid' => $p->id,
+                  ':cid' => $ca->category->id,
+                  ':set_assign'    => $ca->pre_assign,
+                  ':update_assign' => $ca->pre_assign
+               ]);
+
+               if( !$result ) break;
+            }
          }
       }
 
